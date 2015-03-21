eecb26b5a0cb54fed078470266065f0b1baf7c40
enzo <enzo@enzodeMacBook-Pro.local>
2015-03-21 23:06:40 +0800

    add two problems

---
+++ CombinationSum2.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param candidates, a list of integers
+    # @param target, integer
+    # @ return a list of lists of integers
+    def DFS(self, candidates, target, start, valuelist):
+        length = len(candidates)
+        if target == 0 and valuelist not in Solution.ret:
+            return Solution.ret.append(valuelist)
+        for i in range(start, length):
+            if target < candidates[i]:
+                return
+            self.DFS(candidates, target-candidates[i], i+1, valuelist+[candidates[i]])
+
+    def combinationSum(self, candidates, target):
+        if len(candidates) == 0: return 0
+        candidates.sort()
+        Solution.ret = []
+        self.DFS(candidates, target, 0, [])
+        return Solution.ret
+
+if __name__ == '__main__':
+    A = [2, 3, 6, 7]
+    solution = Solution()
+    res = solution.combinationSum(A, 7)
+    print res
